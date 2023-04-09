02fb1a9fb9b2bc84d3f42f61e6e080dc375377c6
OferRose <oferros@campus.technion.ac.il>
2023-04-09 12:34:58 +0300

    I fixed the program

--- print_number.c
+++ print_number.c
@@ -2,8 +2,8 @@
 
 int course_number[6] = {4, 2, 1, 4, 3, 2};
 int main() {
-	for(int i = 5; i >= 0; i--) {
-		printf("%d", course_number[i]);
+	for(int j = 5; j >= 0; j--) {
+		printf("%d", course_number[j]);
 	}
 	printf("\n");
 	return 0;
